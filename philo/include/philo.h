6a5368eb2995de35ebc03f79805aba7b1341cb9d
Romain FONTAINE <romain@Macbook-Pro-de-Romain-2.local>
2023-07-20 21:36:06 -0400

    Makefile ok sans test

---
+++ philo/include/philo.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 20:46:05 by romain            #+#    #+#             */
+/*   Updated: 2023/07/20 21:06:54 by romain           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+
+// COLOR-----------------------------------------------------------------------
+
+# define BCK "\x1B[30m"
+# define RED "\x1B[31m"
+# define GRE "\x1B[32m"
+# define YEL "\x1B[33m"
+# define BLE "\x1B[34m"
+# define MAG "\x1B[35m"
+# define CYA "\x1B[36m"
+# define WHT "\x1B[37m"
+
+// STRUCTURE-------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+#endif
